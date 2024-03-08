0e38cb9cbdec2dc82aeba42a94e74d031849ad45
Azat Valiev <azat@valievventures.com>
2024-03-08 07:25:36 -0600

    fix yaml lsp, setup default schemas

--- lua/master/plugin-config-lsp.lua
+++ lua/master/plugin-config-lsp.lua
@@ -57,10 +57,25 @@ require("mason-lspconfig").setup({
     "sqlls",
     "tailwindcss",
     "vuels",
-    "hydra_lsp"
+    "yamlls"
   },
   handlers = {
     lsp_zero.default_setup,
+    yamlls = function()
+      require("lspconfig").yamlls.setup({
+        settings = {
+          yaml = {
+            schemas = {
+              ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+              ["https://json.schemastore.org/pre-commit-config.json"] = "/.pre-commit-config.*",
+              ["https://json.schemastore.org/catalog-info.json"] = ".backstage/*.yaml",
+              ["https://raw.githubusercontent.com/iterative/dvcyaml-schema/master/schema.json"] = "**/dvc.yaml",
+              ["https://json.schemastore.org/swagger-2.0.json"] = "**/swagger.yaml",
+            },
+          },
+        },
+      })
+    end
   },
 })
 

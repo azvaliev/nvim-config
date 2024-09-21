40bd0722c5fe6f69e8e37a6c63b6d33ef32f67a6
Azat Valiev <azat@valievventures.com>
2024-09-21 14:20:06 -0500

    make neotest-jest working in monorepo?

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -18,15 +18,26 @@ return {
         adapters = {
           require("neotest-vitest"),
           require("neotest-golang"),
-          require('neotest-jest')({
+          require("neotest-jest")({
             jestCommand = "npm test --",
             jestConfigFile = "jest.config.ts",
             env = { CI = true },
             cwd = function(path)
-              return vim.fn.getcwd()
+              -- Start searching from the buffer's directory
+              local current_dir = fn.fnamemodify(path, ":p:h")
+              local root_dir = fn.getcwd()
+
+              while current_dir and current_dir ~= root_dir do
+                if Path:new(current_dir .. "/jest.config.ts"):exists() then
+                  return current_dir
+                end
+                current_dir = fn.fnamemodify(current_dir, ":h")
+              end
+
+              return root_dir -- Stop searching once the root directory is reached
             end,
           }),
-          require('rustaceanvim.neotest')
+          require("rustaceanvim.neotest"),
         }
       })
 

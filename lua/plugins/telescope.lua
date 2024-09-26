a1cb11e34e5ab35970715e8c62e9871b07ea0244
Azat Valiev <azat@valievventures.com>
2024-09-26 07:57:55 -0500

    use fd for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,7 +14,7 @@ return {
       require("telescope").setup({})
 
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>ff", builtin.fd, {})
       vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
       vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})

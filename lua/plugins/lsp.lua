27ddab76dac624d6e99f76f49c5f8863c5e1094c
Azat Valiev <azat@valievventures.com>
2024-10-04 21:41:32 -0500

    force rename shortcut to include leader

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,7 +58,7 @@ return {
         vim.keymap.set('n', 'gs', function() vim.lsp.buf.signature_help() end, opts)
         vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
         vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set('n', 'rn', function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set('n', '<leader>fa', function() vim.lsp.buf.format({async = true}) end, opts)
         vim.keymap.set({ 'n', 'v' }, '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set('n', '<leader>s', function() telescope.lsp_dynamic_workspace_symbols() end, opts)

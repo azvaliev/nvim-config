0009b115ede740e49648c423c57d781816f29262
Azat Valiev <azat.valiev@homee.com>
2025-05-15 08:16:10 -0500

    feat: switch copilot to shift tab

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -10,6 +10,13 @@ return {
          \ | let b:copilot_enabled = v:false
          \ | endif
         ]]
+
+      -- Shift-Tab to accept suggestions
+      vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+      })
+      vim.g.copilot_no_tab_map = true
     end,
   }
 }
